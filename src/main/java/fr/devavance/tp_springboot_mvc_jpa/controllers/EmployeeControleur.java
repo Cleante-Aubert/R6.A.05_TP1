8ec685e87a299016501247e5fb6cb2066a7efd98
Cléante <bouchkhaf@gmail.com>
2024-02-07 12:00:11 +0100

    versionning

--- src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
@@ -1,15 +1,27 @@
 package fr.devavance.tp_springboot_mvc_jpa.controllers;
 
+import fr.devavance.tp_springboot_mvc_jpa.beans.Employee;
 import fr.devavance.tp_springboot_mvc_jpa.dao.IEmployeeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class EmployeeControleur {
 
-    @Autowired
     private IEmployeeDAO employeeDAO;
+    @Autowired
+    public EmployeeControleur(IEmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
 
+    @RequestMapping("/employee")
+    public Employee show_all_employees(Model model){
+        model.addAttribute("Employees",employeeDAO.findAll());
+        return ;
 
+    }
 
 
 
